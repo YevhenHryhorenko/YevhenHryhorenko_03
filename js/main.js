e87f3c2ad16243c70dd49ef959d2d8f712785217
YevhenHryhorenko <116802765+YevhenHryhorenko@users.noreply.github.com>
2023-03-04 14:38:14 +0200

    optimizing code

--- js/main.js
+++ js/main.js
@@ -7,19 +7,20 @@ let userName,
     currentYear,
     counter = 0;
 
-currentYear = new Date().getFullYear();
+currentYear = new Date();
 userName = prompt('Enter your name:').trim();
 userSurname = prompt('Enter your surname:').trim();
 userEmail = prompt('Enter your email:').trim().toLocaleLowerCase();
 
 if (!userEmail.includes('@')) {
     flagEmail = `not valid email <b>${userEmail}</b> (symbol @ not exist)`;
-    userEmail = flagEmail;
 } else if (userEmail.slice(0, 1) == '@') {
-    flagEmail = `not valid email <b>${userEmail}</b> (symbol @ find in first place)`
-    userEmail = flagEmail;;
+    flagEmail = `not valid email <b>${userEmail}</b> (symbol @ find in first place)`;
 } else if (userEmail.slice(-1) == '@') {
     flagEmail = `not valid email <b>${userEmail}</b> (symbol @ find in last place)`;
+}
+
+if (flagEmail !== undefined) {
     userEmail = flagEmail;
 }
 
@@ -27,12 +28,11 @@ do {
     if (counter > 0) {
         alert('Your date of birth is wrong, please, try again');
     }
-    birthYear = prompt('Enter your birth year:').trim();
-    +birthYear;
+    birthYear = +prompt('Enter your birth year:');
     counter++;
-} while (birthYear > new Date().getFullYear() || birthYear < 1900)
+} while ((birthYear > currentYear.getFullYear()) || (birthYear < 1900))
 
-age = currentYear - birthYear;
+age = currentYear.getFullYear() - birthYear;
 
 document.write(`<p>Full name: ${userSurname} ${userName}</p>
 <p>Email: ${userEmail}</p>
